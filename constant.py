76c34d4c850e89d3ab04a0973c81166c08b8889b
Hasnae-bouhmady <71712677+Hasnae-bouhmady@users.noreply.github.com>
2021-07-02 20:48:48 +0100

    Add files via upload

---
+++ constant.py
@@ -0,0 +1,40 @@
+
+
+
+"""Constantes du jeu"""
+
+
+
+
+
+nombre_sprite_cote = 15
+taille_sprite = 40  # en px (carré)
+cote_fenetre = nombre_sprite_cote * taille_sprite
+
+titre_fenetre = "Bomberman"
+
+# chemin des sprites et des textures du jeu
+image_accueil = "images/img.png"
+image_login="images/img4.png"
+
+image_brick = "images/img_3.png"
+image_fond = "images/ap.png"
+image_pillier = "images/img_1.png"
+image_bombe = "images/bonb.png"
+image_sol = "images/img_2.png"
+image_explosion = "images/explodstart.png"
+
+p1_droite = "images/player1d.png"
+p1_gauche = "images/player1g.png"
+p1_haut = "images/player1h.png"
+p1_bas = "images/player1b.png"
+
+p2_droite = "images/player2d.png"
+p2_gauche = "images/player2g.png"
+p2_haut = "images/player2h.png"
+p2_bas = "images/player2b.png"
+
+flamme_d = "images/explod.png"
+flamme_g = "images/explog.png"
+flamme_h = "images/exploh.png"
+flamme_b = "images/explob.png"
